fc97ce5355e1b3c5a5957d75e4403ead658866ad
Anjali Gupta <anjaligta@outlook.com>
2021-05-04 16:38:57 -0400

    added schema

---
+++ server/Schemas/TypeDefs/movieType.js
@@ -0,0 +1,14 @@
+import { GraphQLObjectType, GraphQLString } from 'graphql';
+
+const movieType = new GraphQLObjectType({
+    name: "movieType",
+    fields: () => ({
+        Title: { type: GraphQLString},
+        Year: { type: GraphQLString },
+        imdbID: { type: GraphQLString },
+        Type: { type: GraphQLString},
+        Poster: { type: GraphQLString}
+    })
+})
+
+export default movieType;
