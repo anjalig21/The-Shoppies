c43147095160db88665736c590ebf2257053d42f
Anjali Gupta <anjaligta@outlook.com>
2021-05-04 22:42:37 -0400

    fixed sever

--- server/Schemas/TypeDefs/movieType.ts
+++ server/Schemas/TypeDefs/movieType.ts
@@ -3,11 +3,11 @@ import { GraphQLObjectType, GraphQLString } from 'graphql';
 const movieType = new GraphQLObjectType({
     name: "movieType",
     fields: () => ({
-        Title: { type: GraphQLString},
+        Title: { type: GraphQLString },
         Year: { type: GraphQLString },
         imdbID: { type: GraphQLString },
-        Type: { type: GraphQLString},
-        Poster: { type: GraphQLString}
+        Type: { type: GraphQLString },
+        Poster: { type: GraphQLString }
     })
 })
 
---
+++ server/server.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import movieType from "./Schemas/TypeDefs/movieType";
+import movieSearch from "./Schemas/Interfaces/movieSearch";
+require("dotenv").config();
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const app = express();
+const port: string = process.env.PORT || '5000';
+
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+} from "graphql";
+
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQuery",
+  fields: {
+    movieSearch: {
+      type: new GraphQLList(movieType),
+      args: {
+        title: { type: GraphQLString },
+      },
+      async resolve(parent, args) {
+        const result: movieSearch[] = await axios.get(`http://www.omdbapi.com/?s=${args.title}&apikey=${process.env.KEY}`)
+        .then((res) => {
+          return res.data.Search;
+        })
+        .catch((err: any) => {
+          console.log(err);
+        })
+        return result;
+      }
+    },
+  },
+});
+
+const schema = new GraphQLSchema({ query: RootQuery });
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
