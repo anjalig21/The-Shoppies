31750f9e3ce778fb2601b293cdfe579468e8de29
Anjali Gupta <anjaligta@outlook.com>
2021-05-08 14:09:28 -0400

    added nominations component

---
+++ the-shoppies/src/Components/Nominations/Nominations.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./NominationStyles.css"
+import {Card} from '@shopify/polaris';
+
+
+const Nominations = () => {
+    return (
+        <div className="Nominations">
+            <Card>
+                Results
+            </Card>
+            
+        </div>
+    )
+}
+
+export default Nominations
