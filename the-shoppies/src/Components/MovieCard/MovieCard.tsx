638018e54dcdce7d6e07d69947d0d0cd4b03a1c9
Anjali Gupta <anjaligta@outlook.com>
2021-05-08 17:17:22 -0400

    updated movie card

--- the-shoppies/src/Components/MovieCard/MovieCard.tsx
+++ the-shoppies/src/Components/MovieCard/MovieCard.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import {Card} from '@shopify/polaris';
+import React from "react";
+import { Card, Button } from "@shopify/polaris";
+import "./MovieCard.css";
 
-const MovieCard = (props: { movieTitle: String; movieDescription: String; }) => {
-  const { movieTitle, movieDescription } = props;
+const MovieCard = (props: {
+  movieTitle: string;
+  movieDescription: string;
+  moviePoster: string;
+}) => {
+  const { movieTitle, movieDescription, moviePoster } = props;
   return (
     <div>
-      <Card sectioned title={movieTitle} actions={[{ content: "Nominate" }]}>
-        <p>
-          {movieDescription}
-        </p>
+      <Card sectioned>
+        <div className="cont">
+          <img className="posters" src={moviePoster} />
+          <div className="spacing" />
+          <div>
+            <h2 className="title">{movieTitle}</h2>
+            <p className="info">{movieDescription}</p>
+          </div>
+          <div className="spacingButton"></div>
+          <div className="button">
+            <Button primary>Nominate</Button>
+          </div>
+        </div>
       </Card>
     </div>
   );
